119729e6897e45ee46a8eda47302a8b0decf90de
samad aderinto <81313734+samadaderinto@users.noreply.github.com>
2023-07-22 09:34:27 +0100

    added cart app for product models and views

---
+++ cart/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+from core.utilities import CATEGORIES_CHOICE, LABEL_CHOICE, USPS_SERVICE_CHOICE
+from django.core.validators import MinValueValidator, MaxValueValidator
+from nanoid import generate
+from taggit.managers import TaggableManager
+
+
+
+class Product(models.Model):
+    # store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    title = models.CharField(max_length=110, blank=False, null=False)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=15, decimal_places=2, blank=False, null=False)
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(50)])
+    available = models.IntegerField(
+        null=False, blank=False, validators=[MinValueValidator(0)])
+    category = models.CharField(choices=CATEGORIES_CHOICE, max_length=4)
+    label = models.CharField(choices=LABEL_CHOICE,
+                             max_length=50, default="NEW")
+    product_visibility = models.BooleanField(default=True)
+    image = models.ImageField(
+        upload_to="images", default="", null=True, blank=True)
+
+    average_rating = models.DecimalField(max_digits=4, decimal_places=2, default=0.00, validators=[
+                                         MinValueValidator(0), MaxValueValidator(5)])
+    usps_delivery_date = models.IntegerField(
+        default=0, blank=False, null=False)
+    usps_service = models.CharField(choices=USPS_SERVICE_CHOICE, max_length=20)
+    
+    tags = TaggableManager()
+    sponsored = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def set_availability(self, quantity_bought: int):
+        self.available -= quantity_bought
+    
+    def set_sponsorship(self,switch_to: bool):
+            self.sponsored = switch_to
---
+++ core/utilities.py
@@ -0,0 +1,49 @@
+PAYMENT_STATUS_CHOICE = (
+    ("pnd", "pending"),
+    ("sfl", "successful"),
+    ('fld', "failed")
+)
+
+CATEGORIES_CHOICE = (
+    ("FSHN", "fishing"),
+    ("SPRT", "sports"),
+    ("ELCT", "electronics"),
+    ("PHNS", "phones"),
+    ("GMES", "games"),
+    ("TBLT", "tablets"),
+    ("OUWR", "outwear"),
+    ("PETS", "pets"),
+    ("TOYS", "toys"),
+    ("CMPT", "computing"),
+    ("LNGR", "lingerie"),
+    ("BOOK", "books")
+)
+
+ORDER_STATUS_CHOICE = (
+    ("CND", "cancelled"),
+    ("RFD", "refunded"),
+    ("DLV", "delivered"),
+    ("SHP", "shipped"),
+    ("PKU", "picked up"),
+    ("PND", "pending"),
+    ("CFD", "confirmed")
+)
+
+LABEL_CHOICE = (
+
+    ("NEW", "new"),
+    ("", "none"),
+    ("BTS", "bestseller"),
+    ("SDT", "sold out")
+)
+
+USPS_SERVICE_CHOICE = (
+    ("express", "none"),
+    ("priority", "new"),
+    ("firstclass", "bestseller")
+)
+
+DELIVERY_METHOD_CHOICE = (
+    ("pckup", "pick up"),
+    ("hmdlvry", "home delivery")
+)
