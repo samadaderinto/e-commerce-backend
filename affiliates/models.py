3ddf4033091c2dbaf90655210f7bb843ec6ebcc3
samad aderinto <81313734+samadaderinto@users.noreply.github.com>
2023-08-01 02:35:11 +0100

    building refferal link checker logic

---
+++ affiliates/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from core.utilities import generate_marketer_code
+
+from core.models import User
+from product.models import Product
+from nanoid import generate
+
+# Create your models here.
+
+class Marketer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    marketer_id = models.CharField(max_length=70, default=generate(size=15), unique=True,editable=False)
+    name = models.CharField(max_length=70)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+   
+class Url(models.Model):
+    marketer = models.ForeignKey(Marketer, on_delete=models.CASCADE) 
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    identifier = models.CharField(max_length=120)
+    abs_url = models.URLField()
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+            
+            
+            
+            
+            
+
+
+    
+    
